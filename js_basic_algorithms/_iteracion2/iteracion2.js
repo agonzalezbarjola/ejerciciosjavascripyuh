2fafd53f08130918c47655a897fc94951728eb16
unknown <agonzalezbarjola@gmail.com>
2021-12-09 19:13:11 +0100

    Actualizacion iteracion 2

--- js_basic_algorithms/_iteracion2/iteracion2.js
+++ js_basic_algorithms/_iteracion2/iteracion2.js
@@ -1,4 +1,49 @@
+/* 1.1 */
+
 const character={
     name: "Jack Sparrow",
-    age: 10,
-}
+    age: 10
+};
+
+character["age"]= 25;
+
+console. log (character);
+
+/* 1.2 */
+
+let firstName="Jon";
+let lastName="Snow";
+let age="24";
+
+let frase= "Soy "+firstName+" "+lastName+", tengo "+age+" años y me gustan los lobos.";
+
+console. log(frase);
+
+/* 1.3 */
+
+const toy1={
+    name: "Buss myYear", 
+    price: 19
+};
+
+const toy2={
+    name :"Rayo mcKing",
+    price: 29
+};
+
+let total = toy1["price"]+toy2["price"];
+
+console. log(total)
+
+/* 1.4 */
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice=25000;
+car1["finalPrice"]= globalBasePrice+car1["basePrice"];
+car2["finalPrice"]= globalBasePrice+car2["basePrice"];
+
+console.log(car1)
+console.log(car2)
+
